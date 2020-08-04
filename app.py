2957c282f6dbabb01056a70bb505254bf51f6430
Thomas Petrangelo <thpetran@syr.edu>
2020-08-03 21:56:19 -0400

    Added 3 charts and 3 inputs TODO: add subreddit header and link TODO: figure out how to reset chart after changing subreddit

--- app.py
+++ app.py
@@ -1,49 +1,88 @@
-# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import requests
 import plotly.graph_objs as go
 from collections import deque
 import plotly
-import random
 
 X = deque(maxlen=20)
 X.append(1)
 Y = deque(maxlen=20)
 Y.append(0)
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.JOURNAL])
 
 app.layout = html.Div([
     html.Div([html.H1('Spike'),
               html.H6('Who\'s Where Now'),
-              html.Br(),
-              dcc.Input(id='input-subreddits', type='text', placeholder='Add a subreddit', debounce=True), ],
+              ],
              style={'textAlign': 'center'}),
     html.Hr(),
-    html.Div(id='chosen-subreddit'),
-    dcc.Graph(id='live-graph', animate=True),
+    dbc.Row(
+        [
+            dbc.Col(dcc.Input(id='subreddit-1', type='text', placeholder='Add a subreddit', debounce=True)),
+            dbc.Col(dcc.Input(id='subreddit-2', type='text', placeholder='Add a subreddit', debounce=True)),
+            dbc.Col(dcc.Input(id='subreddit-3', type='text', placeholder='Add a subreddit', debounce=True)),
+        ], style={'textAlign': 'center'}
+    ),
+    dbc.Row(
+        [
+            dbc.Col(
+                html.Div(
+                    dcc.Graph(id='graph-1', animate=True,
+                              style={
+                                  'height': 500,
+                                  'width': 500,
+                              }),
+                ),
+            ),
+            dbc.Col(
+                html.Div(
+                    dcc.Graph(id='graph-2', animate=True,
+                              style={
+                                  'height': 500,
+                                  'width': 500,
+                              })
+                )
+            ),
+            dbc.Col(
+                html.Div(
+                    dcc.Graph(id='graph-3', animate=True,
+                              style={
+                                  'height': 500,
+                                  'width': 500,
+                              })
+                )
+            ),
+        ]
+    ),
     dcc.Interval(
         id='graph-update',
-        interval=2000,
+        interval=15000,
         n_intervals=0
     ),
+    dbc.Row(
+        [
+            dbc.Col(html.Div(id='chosen-subreddit-1')),
+            dbc.Col(html.Div(id='chosen-subreddit-2')),
+            dbc.Col(html.Div(id='chosen-subreddit-3')),
+        ], style={'text-align': 'center'}
+    )
 ])
 
 
 @app.callback(
-    Output('chosen-subreddit', 'children'),
-    [Input('input-subreddits', 'value')])
+    Output('chosen-subreddit-1', 'children'),
+    [Input('subreddit-1', 'value')])
 def subreddit_input(submitted_subreddit):
     return submitted_subreddit
 
 
-@app.callback(Output('live-graph', 'figure'),
-              [Input('graph-update', 'n_intervals'), Input('chosen-subreddit', 'children')])
+@app.callback(Output('graph-1', 'figure'),
+              [Input('graph-update', 'n_intervals'), Input('chosen-subreddit-1', 'children')])
 def update_graph_scatter(n, subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     r = requests.get(url, headers={'user-agent': 'Web App:Spike:v0.0.1: By /u/SpikeDevTom'})
